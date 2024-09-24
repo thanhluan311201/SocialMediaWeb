f7c7e415ffd6e8ed6f7b9156bf1e5fb568822284
thanhluan311201 <thanhluan311201@gmail.com>
2024-09-24 23:23:53 +0700

    update frontend
    finished the login and register page

--- SocialMediaWeb_Frontend/frontend-app/src/App.js
+++ SocialMediaWeb_Frontend/frontend-app/src/App.js
@@ -5,8 +5,15 @@ import Login from './components/User/Login';
 function App() {
   return (
     <div>
-      <Login />
+      <div className='greeting'>
+        <h1>SocialMediaWeb</h1>
+        <h2>The name speaks for itself. Try it out.</h2>
+      </div>
+      <div className='login'>
+        <Login />
+      </div>
     </div>
+    
   );
 }
 
--- SocialMediaWeb_Frontend/frontend-app/src/components/User/Login.jsx
+++ SocialMediaWeb_Frontend/frontend-app/src/components/User/Login.jsx
@@ -15,66 +15,64 @@ const Login = () => {
     }
 
     return (
-        <div className={`wrapper${action}`}>
-            <div className='form-box login'>
-                <form action=''>
-                    <h1>Login</h1>
-                    <div className='input-box'>
-                        <input type='text' placeholder='Username' required/>
-                        <FaUser className='icon'/>
-                    </div>
-                    <div className='input-box'>
-                        <input type='password' placeholder='Password' required/>
-                        <FaLock className='icon'/>
-                    </div>
-                    <div className='remember-forgot'>
-                        <label><input type='checkbox' />Remember me</label>
-                        <a href='/#'>Forgot password?</a>
-                    </div>
+            <div className={`wrapper${action}`}>
+                <div className='form-box login'>
+                    <form action=''>
+                        <h1>Login</h1>
+                        <div className='input-box'>
+                            <input type='text' placeholder='Username' required/>
+                            <FaUser className='icon'/>
+                        </div>
+                        <div className='input-box'>
+                            <input type='password' placeholder='Password' required/>
+                            <FaLock className='icon'/>
+                        </div>
+                        <div className='remember-forgot'>
+                            <a href='/#'>Forgot password?</a>
+                        </div>
 
-                    <button type='submit'>Login</button>
+                        <button type='submit'>Login</button>
 
-                    <div className='register-link'>
-                        <p>Don't have account? <a href='/#' onClick={registerLink}>Register</a></p>
-                    </div>
-                </form>
+                        <div className='register-link'>
+                            <p>Don't have account? <a href='/#' onClick={registerLink}>Register</a></p>
+                        </div>
+                    </form>
+                </div>
+                <div className='form-box register'>
+                    <form action=''>
+                        <h1>Registration</h1>
+                        <div className='input-box'>
+                            <input type='text' placeholder='Username' required/>
+                            <FaUser className='icon'/>
+                        </div>
+                        <div className='input-box'>
+                            <input type='text' placeholder='First name' required/>
+                            <FaUser className='icon'/>
+                        </div>
+                        <div className='input-box'>
+                            <input type='text' placeholder='Last name' required/>
+                            <FaUser className='icon'/>
+                        </div>
+                        <div className='input-box'>
+                            <input type='date' placeholder='Date of birth' required/>
+                            <FaCalendarAlt  className='icon'/>
+                        </div>
+                        <div className='input-box'>
+                            <input type='password' placeholder='Password' required/>
+                            <FaLock className='icon'/>
+                        </div>
+                        <div className='remember-forgot'>
+                            <label><input type='checkbox' />I agree the terms & conditions</label>
+                        </div>
+
+                        <button type='submit'>Register</button>
+
+                        <div className='register-link'>
+                            <p>Already have an account? <a href='/#' onClick={loginLink}>Login</a></p>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div className='form-box register'>
-                <form action=''>
-                    <h1>Registration</h1>
-                    <div className='input-box'>
-                        <input type='text' placeholder='Username' required/>
-                        <FaUser className='icon'/>
-                    </div>
-                    <div className='input-box'>
-                        <input type='text' placeholder='First name' required/>
-                        <FaUser className='icon'/>
-                    </div>
-                    <div className='input-box'>
-                        <input type='text' placeholder='Last name' required/>
-                        <FaUser className='icon'/>
-                    </div>
-                    <div className='input-box'>
-                        <input type='date' placeholder='Date of birth' required/>
-                        <FaCalendarAlt  className='icon'/>
-                    </div>
-                    <div className='input-box'>
-                        <input type='password' placeholder='Password' required/>
-                        <FaLock className='icon'/>
-                    </div>
-                    <div className='remember-forgot'>
-                        <label><input type='checkbox' />I agree the terms & conditions</label>
-                    </div>
-
-                    <button type='submit'>Register</button>
-
-                    <div className='register-link'>
-                        <p>Already have an account? <a href='/#' onClick={loginLink}>Login</a></p>
-                    </div>
-                </form>
-            </div>
-        </div>
-        
     );
 }
 
